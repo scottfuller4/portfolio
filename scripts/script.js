9ec73f670f04052d92abb1568a9d563723a1a5fd
Scott <scottfuller4@gmail.com>
2018-12-14 12:57:25 -0500

    added form and inital scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,32 @@
 const App = {};
 
 $(function () {
+    App.init();
 });
+
+App.init = function () {
+    App.menu();
+    App.contact();
+};
+
+App.menu = function () {
+    $(".hamburger").on("click", function () {
+        $(".pageNav").toggleClass("menuHide");
+    })
+}
+
+App.contact = function () {
+    $(".contactButton").on("click", function () {
+        $(".contactForm").removeClass("menuHide");
+        $("#rotate").rotate({ angle: 0, animateTo: 90, duration: 500 });
+        $("html, body").animate({
+            scrollTop: $(".contactForm").offset().top
+        }, 1000);
+    })
+}
+
+// $("html, body").animate(
+//     {
+//         scrollTop: $(`#qa${bombApp.questionCount}`).offset().top
+//     },
+//     1000);
